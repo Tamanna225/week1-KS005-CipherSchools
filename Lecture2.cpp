fa66ca2af31f632ea1b8a5ee44dbb59033198c6f
Tamanna225 <119917783+Tamanna225@users.noreply.github.com>
2023-10-07 20:12:51 +0530

    Add files via upload

---
+++ Lecture2.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Header file of input output stream is included using this line
+
+using namespace std;  //helpng us to use cout here instead of std::cout
+
+int main()      //This is our main function code starts to execute from here
+{
+    cout<<"Hello world";   //This is our displayed value 
+
+    return 0;      // This is our actual output the value returned by function
+}
