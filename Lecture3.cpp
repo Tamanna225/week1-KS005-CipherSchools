59aab6ba34810ad292d338013e933e95abe6df16
Tamanna225 <119917783+Tamanna225@users.noreply.github.com>
2023-10-07 21:03:22 +0530

    Add files via upload

---
+++ Lecture3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Program for adding two numbers (3 and 5) by assigning those two numbers to two variables a and b
+
+
+
+    int a,b;  // Creation of two integer variables
+    
+
+
+    // Assigning value to variables
+    a = 3;     
+    b = 5;
+
+
+
+    /* There are various ways you can add these
+    elsements and display them like adding them and
+    storing them in new variable and cout that or 
+    you can just directly cout it and to print in next line we
+    use endl which stand for end line and there is vast
+    difference between a+b and "a+b" so use these quotes carefully*/
+
+
+
+    //Declaring c and storing a+b in c
+
+    //int c;
+    //c = a+b;
+    //cout<<c;
+
+
+
+    //You can cout all things in one line
+
+    cout<<a+b<<endl<<"a+b";
+
+
+
+
+    // You can also use multiple cout its all your choice
+
+    //cout<<a+b;
+    //cout<<endl;
+    //cout<<"a+b";
+ 
+    return 0;
+}
